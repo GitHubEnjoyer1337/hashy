e06c6150d7b2f6d1e2a02b61398c96829e53f2e1
mex <you@example.com>
2024-10-14 23:25:12 +0200

    anotherone

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,49 @@
+use core::panic;
 use std::env;
 use sha2::{Digest, Sha256};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (to_hash, times) = parse_args(&args);
-    let hash_string = hash_loop(to_hash, times);
+    let config = Config::new(&args);
+    let hash_string = hashio_loop(config.to_hash, config.count);
 
     println!("the hash is: {}", hash_string);
 }
 
-//struct Config {
-//    to_hash: String,
-//    count: i32,
-//}
-
-fn parse_args(args: &[String]) -> (&str, i32) {
-    let to_hash = &args[1];
-    let times: i32 = match args[2].parse() {
-        Ok(num) => num,
-        Err(_) => {
-            eprintln!("second arg must be valid int");
-            std::process::exit(1);
-        }
-    };
-//    Config {
-//        to_hash,
-//        count: times,
-//    }
-    (to_hash, times)
+struct Config<'a> {
+    to_hash: &'a str,
+    count: usize,
 }
 
-fn hash_loop(input: &str, count: i32) -> String {
+impl<'a> Config<'a> {
+    fn new(args: &'a Vec<String>) -> Result<Config<'a>, &'static str> {
+        if args.len() != 3 {
+            return Err("Usage: <program> <string> <count>");
+        }
+        let to_hash = args[1].as_str();
+        let count = 
+            match args[2].parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    eprintln!("second arg must be valid int (usize)");
+                    std::process::exit(1);
+                }
+            };
+        Ok(Config { to_hash, count })
+    }
+}
+// just made this loop in prep for interactive functionality
+fn hashappend_loop(input: &str, count: usize) -> String {
+    let mut hasher = Sha256::new();
+    let mut inputstr = input.to_string();
+    inputstr.push_str(&input.repeat(count as usize));
+    hasher.update(&inputstr);
+    let result = hasher.finalize();
+
+    format!("{:x}", result)
+}
+
+fn hashio_loop(input: &str, count: usize) -> String {
 
     let mut hasher = Sha256::new();
     hasher.update(input);
