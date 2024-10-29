06374bc97f920d37260a9f0d11ffaaabaeaf0cad
user01 <maxkellermayr@gmail.com>
2024-10-29 12:56:16 +0100

    fixed output and arg count if args<4

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match run(args) {
-        Ok(result) => println!("{:?}", result),
+        Ok(result) => match result {
+            testrust::HashResult::StringResult(string) => println!("{}", string),
+            testrust::HashResult::TupleResult(string, number) => println!("{} {}", string, number),
+        }
         Err(e) => eprintln!("Error: {}", e),
     }
 }
